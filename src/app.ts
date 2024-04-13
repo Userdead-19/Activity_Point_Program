0d66ae5cb7464e0963cfce57d9c21c0db39641ac
UserDead-19 <abinav10102@gmail.com>
2024-04-13 23:13:30 +0530

    Add ALLOWED_ORIGINS environment variable and rate limiting middleware

--- src/app.ts
+++ src/app.ts
@@ -8,15 +8,41 @@ import UserRouter from './Router/UserRouter';
 import MessageResponse from './interfaces/MessageResponse';
 import IssuesRouter from './Router/IssuesRouter';
 import AdminRouter from './Router/adminRouter';
+import { rateLimit, Store } from 'express-rate-limit'
+
 
 require('dotenv').config();
 const swaggerSpecs = require('../swaggerConfig');
 const swaggerUi = require('swagger-ui-express');
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 100,
+  message: 'Too many requests, please try again later',
+  validate: true,
+  standardHeaders: 'draft-6',
+
+})
+
+
 app.use(morgan('dev'));
-app.use(helmet());
-app.use(cors());
+app.use(helmet({
+  contentSecurityPolicy: false,
+}
+));
+app.use(cors(
+  {
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (process.env.ALLOWED_ORIGINS?.split(',').includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  }
+));
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
